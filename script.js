b3da7af4c89e781b2453e8b1d77f66dbc4bdf774
Nguyen Van Luong <luongnv89@gmail.com>
2014-10-22 11:26:14 +0200

    Fix impact version

--- script.js
+++ script.js
@@ -239,24 +239,24 @@ luongnvinfo={
 		rsP = document.getElementById('previousName');
 		rsN = document.getElementById('nextName');
 		btnNext = document.getElementById('btnNext');
-		btnTime = document.getElementById('btnTime');
+		// btnTime = document.getElementById('btnTime');
 		rsVolume = document.getElementById('volume');
 		rsVolume.value = audioplayer.volume*100;
 		rsVolume.addEventListener('change',luongnvinfo.setVolume,false);
 		volumeValue = document.getElementById('volumeValue');
 		volumeValue.innerHTML = rsVolume.value/10;
-		btnTime.onclick=function () {
-			var time = prompt('Set time to stop (minutes)',10);
-			if(time<0&&!(time instanceof Number)){
-				alert('Wrong input');
-			}else{
-				window.setTimeout(function () {
-					audioplayer.remove();
-					var content = document.getElementById('main-contain');
-					content.innerHTML="<h1 id='thankText'>Thank you ... !</h1>";
-				},time*60000);
-			}
-		}
+		// btnTime.onclick=function () {
+		// 	var time = prompt('Set time to stop (minutes)',10);
+		// 	if(time<0&&!(time instanceof Number)){
+		// 		alert('Wrong input');
+		// 	}else{
+		// 		window.setTimeout(function () {
+		// 			audioplayer.remove();
+		// 			var content = document.getElementById('main-contain');
+		// 			content.innerHTML="<h1 id='thankText'>Thank you ... !</h1>";
+		// 		},time*60000);
+		// 	}
+		// }
 		btnNext.onclick = function () {
 			var currentIndex = audioplayer.getAttribute('currentRS');
 			if(currentIndex>=luongnvinfo.data.length-1){
