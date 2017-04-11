cfad9658c9385d1f2f7fa2d4f8e09efa912e1542
Luong NGUYEN <luongnv89@gmail.com>
2017-04-11 16:46:46 +0200

    move volume text to right

--- js/radio.js
+++ js/radio.js
@@ -4,7 +4,7 @@
  * @copyright luongnv89
  */
 radio = {
-    version: '1.0.2.3',
+    version: '1.0.2.0',
     langs: ['English', 'French', 'Instrument', 'Other'],
     cats: ['News', 'Music', 'Talk'],
     data: [],
