a6f6964d11f1a411dfedd6665c6c21f5bd426f17
Luong NGUYEN <luongnv89@gmail.com>
2017-04-11 16:59:46 +0200

    update style

--- js/radio.js
+++ js/radio.js
@@ -4,7 +4,7 @@
  * @copyright luongnv89
  */
 radio = {
-    version: '1.0.2.0',
+    version: '1.0.2.1',
     langs: ['English', 'French', 'Instrument', 'Other'],
     cats: ['News', 'Music', 'Talk'],
     data: [],
