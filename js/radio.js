b541bbc16ee011e927dcbd64c295e0f2cafb73e0
Luong NGUYEN <luongnv89@gmail.com>
2017-04-11 16:27:29 +0200

    use btn-default

--- js/radio.js
+++ js/radio.js
@@ -4,7 +4,7 @@
  * @copyright luongnv89
  */
 radio = {
-    version: '1.0.0.1',
+    version: '1.0.0.2',
     langs: ['English', 'French', 'Instrument', 'Other'],
     cats: ['News', 'Music', 'Talk'],
     data: [],
